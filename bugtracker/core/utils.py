991d104789fe828d212d0222b9b21ee9240dddbe
dhogg <danielhogg@protonmail.com>
2019-11-12 16:35:36 -0500

    added GPLv3 license

--- bugtracker/core/utils.py
+++ bugtracker/core/utils.py
@@ -1,4 +1,22 @@
 """
+This file is part of Bugtracker
+Copyright (C) 2019  McGill Radar Group
+
+Bugtracker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bugtracker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bugtracker.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
 Miscellaneous utility functions/classes thrown into
 a utility module. If this grows too big, split it up
 into multiple modules.
