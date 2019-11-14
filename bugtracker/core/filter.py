f99d78c30d37be3d076c605c00ee13dd64707733
Daniel Hogg <daniel.hogg@mcgill.ca>
2019-11-14 17:24:43 -0500

    refactoring filters

--- bugtracker/core/filter.py
+++ bugtracker/core/filter.py
@@ -47,32 +47,8 @@ class Filter():
         self.vertical_angles = None
 
 
-    def verify_dims(self):
-        
-        if self.dbz_3d is None:
-            raise ValueError("dbz_3d not set")
-
-        if self.filter_3d is None:
-            raise ValueError("filter_3d not set")
-
-        dbz_shape = self.dbz_3d.shape
-        filter_shape = self.dbz_3d.shape
-
-        if len(dbz_shape) != 3 or len(filter_shape) != 3:
-            raise ValueError("3D array expected.")
-
-        if dbz_shape != filter_shape:
-            raise ValueError(f"Incompatible numpy arrays: {dbz_shape}, {filter_shape}")
-
-        num_angles = dbz_shape[0]
-        num_azims = dbz_shape[1]
-        num_gates = dbz_shape[2]
-
-        if num_angles != len(self.vertical_angles):
-            raise ValueError()
-
-        if num_azims != self.grid_info.azims:
-            raise ValueError(f"Incompatible azims: {azims}, {self.grid_info.azims}")
+    def check_dims(self):
+        pass
 
 
     def set_filter(self, fill_entry=True):
@@ -91,13 +67,11 @@ class Filter():
         self.filter_3d.fill(fill_entry)
 
 
-    def setup(self, dbz_3d, angles):
+    def setup(self, angles):
 
-        self.dbz_3d = dbz_3d
         self.vertical_angles = angles
         self.set_filter(fill_entry=True)
-        self.verify_dims()
-
+        self.check_dims()
 
 
 """
--- bugtracker/core/precip.py
+++ bugtracker/core/precip.py
@@ -23,3 +23,36 @@ class PrecipFilter(Filter):
 
     def __init__(self, metadata, grid_info):
         super().__init__(metadata, grid_info)
+
+
+    def verify_dims(self):
+        
+        if self.dbz_3d is None:
+            raise ValueError("dbz_3d not set")
+
+        if self.filter_3d is None:
+            raise ValueError("filter_3d not set")
+
+        dbz_shape = self.dbz_3d.shape
+        filter_shape = self.dbz_3d.shape
+
+        if len(dbz_shape) != 3 or len(filter_shape) != 3:
+            raise ValueError("3D array expected.")
+
+        if dbz_shape != filter_shape:
+            raise ValueError(f"Incompatible numpy arrays: {dbz_shape}, {filter_shape}")
+
+        num_angles = dbz_shape[0]
+        num_azims = dbz_shape[1]
+        num_gates = dbz_shape[2]
+
+        if num_angles != len(self.vertical_angles):
+            raise ValueError()
+
+        if num_azims != self.grid_info.azims:
+            raise ValueError(f"Incompatible azims: {azims}, {self.grid_info.azims}")
+
+
+    def set_dbz(self):
+        # Set dbz array
+        pass
