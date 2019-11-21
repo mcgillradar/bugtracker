1822fbd881b428d0ef70ec4836bda73479ade825
Daniel Hogg <daniel.hogg@mcgill.ca>
2019-11-21 11:57:17 -0500

    inheritance classes -> scoped Processor

--- bugtracker/io/processor.py
+++ bugtracker/io/processor.py
@@ -40,6 +40,7 @@ class Processor(abc.ABC):
             raise FileNotFoundError("Missing calib file")
 
         self.load_universal_calib()
+        self.verify_universal_calib()
 
 
     def load_universal_calib(self):
@@ -49,7 +50,30 @@ class Processor(abc.ABC):
         """
         
         calib_file = self.calib_file
-        dset = nc.Dataset()
+
+        dset = nc.Dataset(calib_file, mode='r')
+
+        self.lats = dset.variables['lats'][:,:]
+        self.lons = dset.variables['lons'][:,:]
+        self.altitude = dset.variables['altitude'][:,:]
+
+        dset.close()
+
+    def verify_universal_calib(self):
+        """
+        Make sure all the dimensions are correct
+        """
+
+        polar_dims = (self.grid_info.azims, self.grid_info.gates)
+
+        if self.lats.shape != polar_dims:
+            raise ValueError(f"Lat grid invalid dims: {self.lats.shape}")
+        
+        if self.lons.shape != polar_dims:
+            raise ValueError(f"Lon grid invalid dims: {self.lons.shape}")
+
+        if self.altitude.shape != polar_dims:
+            raise ValueError(f"Altitude grid invalid dims: {self.altitude.shape}")
 
 
     @abc.abstractmethod
@@ -60,16 +84,60 @@ class Processor(abc.ABC):
         """
         pass
 
+    @abc.abstractmethod
+    def verify_specific_calib(self):
+        """
+        Verify the dimensions are correct for filetype specific
+        parts of the calibration
+        """
+        pass
+
 
 class IrisProcessor(Processor):
 
     def __init__(self, metadata, grid_info):
         super().__init__(metadata, grid_info)
         self.load_specific_calib()
-
+        self.verify_specific_calib()
 
     def load_specific_calib(self):
-        pass
+        """
+        Iris-specific calibration data includes CONVOL/DOPVOL
+        """
+
+        calib_file = self.calib_file
+        dset = nc.Dataset(calib_file, mode='r')
+
+        self.convol_angles = dset.variables['convol_angles'][:]
+        self.dopvol_angles = dset.variables['dopvol_angles'][:]
+        self.convol_clutter = dset.variables['convol_clutter'][:,:,:]
+        self.dopvol_clutter = dset.variables['dopvol_clutter'][:,:,:]
+
+        dset.close()
+
+    def verify_specific_calib(self):
+        
+        num_convol = len(self.convol_angles)
+        num_dopvol = len(self.dopvol_angles)
+
+        if num_convol <= 1:
+            raise ValueError("Not enough CONVOL angles in calib")
+
+        if num_dopvol <= 1:
+            raise ValueError("Not enough DOPVOL angles in calib")
+
+        azims = self.grid_info.azims
+        gates = self.grid_info.gates
+
+        convol_dim = (num_convol, azims, gates)
+        dopvol_dim = (num_dopvol, azims, gates)
+
+        if self.convol_clutter.shape != convol_dim:
+            raise ValueError(f"Invalid convol dimensions: {self.convol_clutter.shape}")
+
+        if self.dopvol_clutter.shape != dopvol_dim:
+            raise ValueError(f"Invalid dopvol dimensions: {self.dopvol_clutter.shape}")
+
 
 class OdimProcessor(Processor):
     """
@@ -79,6 +147,7 @@ class OdimProcessor(Processor):
     def __init__(self):
 
         super().__init__()
+        raise NotImplmentedError("OdimProcessor")
 
 
 class NexradProcessor(Processor):
@@ -89,3 +158,4 @@ class NexradProcessor(Processor):
     def __init__(self):
 
         super().__init__()
+        raise NotImplmentedError("NexradProcessor")
