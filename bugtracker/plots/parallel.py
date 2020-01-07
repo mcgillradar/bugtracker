7954972a98902499d8d8545a1fdb6815248a0f4f
Daniel Hogg <daniel.hogg@mcgill.ca>
2020-01-07 16:02:07 -0500

    finalized parallel plotting

--- bugtracker/plots/parallel.py
+++ bugtracker/plots/parallel.py
@@ -51,14 +51,13 @@ def plot_worker(plot_type, metadata, config, lats, lons, dbz_idx, iris_data, id_
     if plot_type == 'filtered':
         plot_level(plotter, metadata, config, iris_data, dbz_idx, filtered=True)
     elif plot_type == 'unfiltered':
-        print(plot_type)
+        plot_level(plotter, metadata, config, iris_data, dbz_idx, filtered=False)
     elif plot_type == 'joint':
-        print(plot_type)
+        plot_joint_product(plotter, metadata, config, iris_data)
     elif plot_type == 'target_id':
-        print(plot_type)
+        plot_target_id(plotter, metadata, config, iris_data, dbz_idx, id_matrix)
     else:
        raise ValueError(f"Unrecognizable plot type: {plot_type}")
-
 
 
 def plot_level(plotter, metadata, config, iris_data, dbz_idx, filtered=True):
@@ -88,6 +87,36 @@ def plot_level(plotter, metadata, config, iris_data, dbz_idx, filtered=True):
     plotter.save_plot(min_value=-15.0, max_value=40.0)
 
 
+def plot_target_id(id_plotter, metadata, config, iris_data, dbz_idx, id_matrix):
+    """
+    Creating TargetIdPlotter from RadialPlotter
+    """
+
+    max_range = config["plot_settings"]["max_range"]
+
+    prefix = "target_id"
+    dbz_elevs = iris_data.dbz_elevs
+    num_dbz_elevs = len(dbz_elevs)
+
+    elev = dbz_elevs[dbz_idx]
+    data = id_matrix[dbz_idx,:,:]
+    label = f"{prefix}_angle_{elev:.1f}"
+    print(f"Plotting: {label}")
+    id_plotter.set_data(data, label, iris_data.datetime, metadata, max_range)
+    id_plotter.save_plot()
+
+
+def plot_joint_product(plotter, metadata, config, iris_data):
+
+    max_range = config["plot_settings"]["max_range"]
+
+    data = iris_data.joint_product[:,:]
+    label = f"joint_product"
+    print(f"Plotting: {label}")
+    plotter.set_data(data, label, iris_data.datetime, metadata, max_range)
+    plotter.save_plot(min_value=-15.0, max_value=40.0)
+
+
 class ParallelPlotter:
     """
     multiprocessing.Pool based class that allows mupliple plots to
@@ -110,51 +139,23 @@ class ParallelPlotter:
 
         for idx in range(0, num_elevs):
             plot_type = "filtered"
+            arglist = (plot_type, metadata, self.config, lats, lons, idx, iris_data, None)
+            args.append(arglist)
+
+        for idx in range(0, num_elevs):
+            plot_type = "unfiltered"
+            arglist = (plot_type, metadata, self.config, lats, lons, idx, iris_data, None)
+            args.append(arglist)
+
+        for idx in range(0, num_elevs):
+            plot_type = "target_id"
             arglist = (plot_type, metadata, self.config, lats, lons, idx, iris_data, id_matrix)
             args.append(arglist)
 
+        # Only one vertical level here (as it's all put into one level)
+        joint_args = ("joint", metadata, self.config, lats, lons, None, iris_data, None)
+        args.append(joint_args)
 
         self.pool = mp.Pool()
         self.pool.starmap(plot_worker, args)
-
-
-
-
-
-    def plot_joint_product(self, plotter):
-
-        max_range = self.config["plot_settings"]["max_range"]
-
-        data = iris_data.joint_product[:,:]
-        label = f"joint_product"
-        print(f"Plotting: {label}")
-        self.plotter.set_data(data, label, iris_data.datetime, self.metadata, max_range)
-        self.plotter.save_plot(min_value=-15.0, max_value=40.0)
-
-
-    def plot_target_id(self, plotter):
-        """
-        Creating TargetIdPlotter from RadialPlotter
-        """
-
-        lats = self.plotter.lats
-        lons = self.plotter.lons
-        output_folder = self.plotter.output_folder
-
-        id_plotter = bugtracker.plots.identify.TargetIdPlotter(lats, lons, output_folder)
-
-        max_range = self.config["plot_settings"]["max_range"]
-
-        prefix = "target_id"
-        dbz_elevs = iris_data.dbz_elevs
-        num_dbz_elevs = len(dbz_elevs)
-
-        for x in range(0, num_dbz_elevs):
-            elev = dbz_elevs[x]
-            data = id_matrix[x,:,:]
-            label = f"{prefix}_angle_{elev:.1f}"
-            print(f"Plotting: {label}")
-            id_plotter.set_data(data, label, iris_data.datetime, self.metadata, max_range)
-            id_plotter.save_plot()
-
 
